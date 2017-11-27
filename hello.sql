c9bb5c6623fd8aa9cb84df7e0e216b2fc7607f11
irfanijaz <irfanijaz28@gmail.com>
2017-11-27 20:47:06 +0100

    testing jenkins

--- hello.sql
+++ hello.sql
@@ -1,16 +1,16 @@
-create table Customer
+create table Customer1
 (
   CustomerID Integer
   ,CustomerName Varchar(20)
   ,CustomerAge Integer
 );
 
-insert into Customer values (123,'irfan',28);
+insert into Customer1 values (123,'irfan',28);
 
 Create or Replace procedure Sample_Jenkins
 is
 begin
 
-update customer set age = 29;
+update customer1 set age = 29;
 
 end;
