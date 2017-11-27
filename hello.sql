984a0396fa6b42b25aef02508110679fede072f7
irfanijaz <irfanijaz28@gmail.com>
2017-11-27 20:40:00 +0100

    testing jenkins

--- hello.sql
+++ hello.sql
@@ -1,2 +1,16 @@
-INSERT INTO HELLO VALUES(1);
-INSERT INTO HELLO VALUES(2);
+create table Customer
+(
+  CustomerID Integer
+  ,CustomerName Varchar(20)
+  ,CustomerAge Integer
+);
+
+insert into Customer values (123,'irfan',28);
+
+Create or Replace procedure Sample_Jenkins
+is
+begin
+
+update customer set age = 29;
+
+end;
