14f413ad41cbbe78055af5d3e29ee2bd5004444e
irfanijaz <irfanijaz28@gmail.com>
2017-11-27 20:54:51 +0100

    ddl

--- hello.sql
+++ hello.sql
@@ -1,17 +1,7 @@
-create table Customer2
+create table Customer
 (
   CustomerID Integer
   ,CustomerName Varchar(20)
   ,CustomerAge Integer
 );
 
-insert into Customer2 values (123,'irfan',28);
-commit;
-
-Create or Replace procedure Sample_Jenkins
-is
-begin
-
-update customer2 set age = 29;
-
-end;
