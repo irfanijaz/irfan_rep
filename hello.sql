9ffdcd7bb64fae54e7fd2dc26c22eadd4e119c19
irfanijaz <irfanijaz28@gmail.com>
2017-11-27 20:51:16 +0100

    party

--- hello.sql
+++ hello.sql
@@ -5,13 +5,13 @@ create table Customer2
   ,CustomerAge Integer
 );
 
-insert into Customer1 values (123,'irfan',28);
+insert into Customer2 values (123,'irfan',28);
 commit;
 
 Create or Replace procedure Sample_Jenkins
 is
 begin
 
-update customer1 set age = 29;
+update customer2 set age = 29;
 
 end;
